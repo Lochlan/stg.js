4634a69ef93de777d477e53f7d2611d6aa4c8fa6
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2016-06-14 01:35:43 -0700

    Add test for moveGameObjects method

--- src/game.spec.js
+++ src/game.spec.js
@@ -1,5 +1,7 @@
 let Game = require('./game');
 
+let GameObjects = require('./game-objects/game-objects');
+
 describe('Game', function () {
     let game;
 
@@ -12,13 +14,33 @@ describe('Game', function () {
     });
 
     describe('when firing a bullet', function () {
-
         beforeEach(function () {
             game.fireBullet();
         });
 
         it('should add a bullet to the game state', function () {
             expect(game.state.bullets.data[0]).toBeDefined();
+        });
+    });
+
+    describe('when moving game objects', function () {
+        let spies;
+
+        beforeEach(function () {
+            spies = [];
+            let gameObjects = new GameObjects();
+            for (let i=0; i<3; i++) {
+                let spy = jasmine.createSpy();
+                gameObjects.add({move: spy});
+                spies.push(spy);
+            }
+            game.moveGameObjects(gameObjects);
+        });
+
+        it('should invoke the "move" method on each game object', function () {
+            spies.forEach(function (spy) {
+                expect(spy).toHaveBeenCalled();
+            });
         });
     });
 });
