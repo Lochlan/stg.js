ad3158c81c4dfd03ae9973730ab73431760ca0af
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2016-06-20 23:17:27 -0700

    Add event queue tests

---
+++ src/event-queue.spec.js
@@ -0,0 +1,18 @@
+let eventQueue = require('./event-queue');
+let Game = require('./game');
+
+describe('Event queue', function () {
+    describe('when processed by the game', function () {
+        let game;
+
+        beforeEach(function () {
+            game = new Game({eventQueue: eventQueue});
+        });
+
+        it('should process every event without throwing an error', function () {
+            eventQueue.forEach(function (event) {
+                expect(event.procedure.bind(game)).not.toThrow();
+            });
+        });
+    });
+});
